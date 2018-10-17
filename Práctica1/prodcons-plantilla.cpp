33233443f7c398c124024033bae108f077daa5a8
elenamerelo <nenamer@gmail.com>
2018-10-17 10:28:52 +0200

    Finalizando práctica 1

---
+++ Práctica1/prodcons-plantilla.cpp
@@ -0,0 +1,174 @@
+#include <iostream>
+#include <cassert>
+#include <thread>
+#include <mutex>
+#include <random>
+#include "Semaphore.h"
+
+using namespace std ;
+using namespace SEM ;
+
+//**********************************************************************
+// variables compartidas
+
+const int num_items = 75 ,   // número de items
+					primera_celda_ocupada_FIFO= 0, // primera celda ocupada en el vector. Se incrementa al leer
+					primera_celda_libre= 0, //se incrementa al leer
+					primera_celda_ocupada_LIFO= 0, // se incrementa al leer y se decrementa al escribir
+	       	tam_vec   = 15 ;   // tamaño del buffer
+unsigned  cont_prod[num_items] = {0}, // contadores de verificación: producidos
+					buffer[tam_vec]= {0},
+          cont_cons[num_items] = {0}; // contadores de verificación: consumidos
+Semaphore ocupadas= 0;	//número de entradas ocupadas del búffer (#E - #L)
+Semaphore libres= tam_vec; //número de entradas libres del búffer (k + #L - #E), con k el tamaño fijo del búffer
+
+
+//**********************************************************************
+// plantilla de función para generar un entero aleatorio uniformemente
+// distribuido entre dos valores enteros, ambos incluidos
+// (ambos tienen que ser dos constantes, conocidas en tiempo de compilación)
+//----------------------------------------------------------------------
+
+template< int min, int max > int aleatorio() {
+  static default_random_engine generador( (random_device())() );
+  static uniform_int_distribution<int> distribucion_uniforme( min, max ) ;
+  return distribucion_uniforme( generador );
+}
+
+//**********************************************************************
+// funciones comunes a las dos soluciones (fifo y lifo)
+//----------------------------------------------------------------------
+
+int producir_dato(){
+   static int contador = 0 ;
+   this_thread::sleep_for( chrono::milliseconds( aleatorio<20,100>() ));
+
+   cout << "producido: " << contador << endl << flush ;
+
+   cont_prod[contador] ++ ;
+   return contador++ ;
+}
+//----------------------------------------------------------------------
+
+void consumir_dato( unsigned dato ){
+   assert( dato < num_items );
+   cont_cons[dato] ++ ;
+   this_thread::sleep_for( chrono::milliseconds( aleatorio<20,100>() ));
+
+   cout << "                  consumido: " << dato << endl ;
+
+}
+
+
+//----------------------------------------------------------------------
+
+void test_contadores(){
+	bool ok = true ;
+	cout << "comprobando contadores ...." ;
+
+	for( unsigned i = 0 ; i < num_items ; i++ ){
+		if ( cont_prod[i] != 1 ){
+			cout << "error: valor " << i << " producido " << cont_prod[i] << " veces." << endl ;
+			ok = false ;
+		}
+
+		if ( cont_cons[i] != 1 ){
+			cout << "error: valor " << i << " consumido " << cont_cons[i] << " veces" << endl ;
+			ok = false ;
+		}
+	}
+
+	if (ok)
+		cout << endl << flush << "solución (aparentemente) correcta." << endl << flush ;
+}
+
+void mostrar_buffer(){
+	for(unsigned i= 0; i< tam_vec; i++)
+		cout << buffer[i] << " ";
+}
+
+//----------------------------------------------------------------------
+
+void  funcion_hebra_productora_LIFO(){
+	int a;
+	for( unsigned i = 0 ; i < num_items ; i++ ){
+	  a = producir_dato() ;
+	  sem_wait(libres); //esperamos a que pueda escribir
+		//escribimos el dato en a, sentencia e
+		cout << "\nEscribimos " << a << ".Buffer antes: ";
+		mostrar_buffer();
+		buffer[primera_celda_ocupada_LIFO]= a;
+		primera_celda_ocupada_LIFO++;
+		cout << "\nBuffer después: ";
+		mostrar_buffer();
+		sem_signal(ocupadas); //indicamos que ya se puede leer
+	}
+}
+
+//----------------------------------------------------------------------
+
+void funcion_hebra_consumidora_LIFO(){
+	int b;
+	for( unsigned i = 0 ; i < num_items ; i++ ){
+		sem_wait(ocupadas);
+		//leemos b del buffer, sentencia l
+		b= buffer[primera_celda_ocupada_LIFO];
+		primera_celda_ocupada_LIFO--;
+		sem_signal(libres);
+		consumir_dato(b) ;
+	}
+}
+
+
+//----------------------------------------------------------------------
+
+void  funcion_hebra_productora_FIFO(){
+	int a;
+	for( unsigned i = 0 ; i < num_items ; i++ ){
+	  a = producir_dato() ;
+	  sem_wait(libres); //esperamos a que pueda escribir
+		buffer[primera_celda_libre]= a;
+		primera_celda_libre++;
+		sem_signal(ocupadas); //indicamos que ya se puede leer
+	}
+}
+
+//----------------------------------------------------------------------
+
+void funcion_hebra_consumidora_FIFO(){
+	int b;
+	for( unsigned i = 0 ; i < num_items ; i++ ){
+		sem_wait(ocupadas);
+		//leer a de vec, sentencia l
+		sem_signal(libres);
+		consumir_dato(b) ;
+	}
+}
+//----------------------------------------------------------------------
+
+void main(){
+   cout << "--------------------------------------------------------" << endl
+        << "Problema de los productores-consumidores (solución FIFO)." << endl
+        << "--------------------------------------------------------" << endl
+        << flush ;
+
+   thread hebra_productora ( funcion_hebra_productora_FIFO ),
+          hebra_consumidora( funcion_hebra_consumidora_FIFO);
+
+   hebra_productora.join() ;
+   hebra_consumidora.join() ;
+
+   test_contadores();
+
+	 cout << "--------------------------------------------------------" << endl
+				<< "Problema de los productores-consumidores (solución LIFO)." << endl
+				<< "--------------------------------------------------------" << endl
+				<< flush ;
+
+	 hebra_productora= thread( funcion_hebra_productora_LIFO ), hebra_consumidora= thread( funcion_hebra_consumidora_LIFO);
+
+	 hebra_productora.join() ;
+	 hebra_consumidora.join() ;
+
+	 test_contadores();
+}
