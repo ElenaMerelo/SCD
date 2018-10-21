87f9ddb8f9d030c25d3d75f666e4edcb6c3f01be
elenamerelo <nenamer@gmail.com>
2018-10-21 11:20:38 +0200

    Solución n productores n consumidores hecha

---
+++ Práctica1/nprods_ncons_FIFO.cpp
@@ -0,0 +1,165 @@
+#include <iostream>
+#include <cassert>
+#include <thread>
+#include <mutex>
+#include <random>
+#include <future>
+#include "Semaphore.h"
+
+using namespace std ;
+using namespace SEM ;
+
+//**********************************************************************
+// variables compartidas
+
+const int num_items = 75 ,   // número de items
+	       	tam_vec   = 15 ;   // tamaño del buffer
+int				primera_celda_ocupada_FIFO= 0, // primera celda ocupada en el vector. Se incrementa al leer
+					producidos= 0, consumidos= 0,
+					primera_celda_libre= 0; //se incrementa al leer
+unsigned  cont_prod[num_items] = {0}, // contadores de verificación: producidos
+					buffer[tam_vec]= {0},
+          cont_cons[num_items] = {0}; // contadores de verificación: consumidos
+Semaphore puede_consumir= 0;	//número de entradas ocupadas del búffer (#E - #L)
+Semaphore puede_producir= tam_vec; //número de entradas libres del búffer (k + #L - #E), con k el tamaño fijo del búffer
+Semaphore alguien_consumiendo= 1, alguien_produciendo= 1;	//ambos a 1 dado que al principio no hay nadie consumiendo ni produciendo, para que puedan empezar a hacerlo
+mutex mtx;
+
+//**********************************************************************
+// plantilla de función para generar un entero aleatorio uniformemente
+// distribuido entre dos valores enteros, ambos incluidos
+// (ambos tienen que ser dos constantes, conocidas en tiempo de compilación)
+//----------------------------------------------------------------------
+
+template< int min, int max > int aleatorio() {
+  static default_random_engine generador( (random_device())() );
+  static uniform_int_distribution<int> distribucion_uniforme( min, max ) ;
+  return distribucion_uniforme( generador );
+}
+
+//**********************************************************************
+// funciones comunes a las dos soluciones (fifo y lifo)
+//----------------------------------------------------------------------
+
+int producir_dato(){
+   static int contador = 0 ;
+   this_thread::sleep_for( chrono::milliseconds( aleatorio<20,100>() ));
+
+	 mtx.lock();
+   cout << "producido: " << contador << endl << flush ;
+	 mtx.unlock();
+
+   cont_prod[contador] ++ ;
+   return contador++ ;
+}
+//----------------------------------------------------------------------
+
+void consumir_dato( unsigned dato ){
+   assert( dato < num_items );
+   cont_cons[dato] ++ ;
+   this_thread::sleep_for( chrono::milliseconds( aleatorio<20,100>() ));
+
+	 mtx.lock();
+   cout << "                  consumido: " << dato << endl ;
+	 mtx.unlock();
+}
+
+
+//----------------------------------------------------------------------
+
+void test_contadores(){
+	bool ok = true ;
+	cout << "comprobando contadores ...." ;
+
+	for( unsigned i = 0 ; i < num_items ; i++ ){
+		if ( cont_prod[i] != 1 ){
+			cout << "error: valor " << i << " producido " << cont_prod[i] << " veces." << endl ;
+			ok = false ;
+		}
+
+		if ( cont_cons[i] != 1 ){
+			cout << "error: valor " << i << " consumido " << cont_cons[i] << " veces" << endl ;
+			ok = false ;
+		}
+	}
+
+	if (ok)
+		cout << endl << flush << "solución (aparentemente) correcta." << endl << flush ;
+}
+
+void mostrar_buffer(){
+	for(unsigned i= 0; i< tam_vec; i++)
+		cout << buffer[i] << " ";
+}
+
+//----------------------------------------------------------------------
+
+void  funcion_hebra_productora_FIFO(){
+	int a;
+
+	while(producidos < num_items){
+		mtx.lock(); //para aumentar el número de datos producidos en exclusión mutua
+		producidos++;
+		mtx.unlock();
+
+		sem_wait(puede_producir); //esperamos a que pueda escribir
+
+		sem_wait(alguien_produciendo); // cuando haya alguien produciendo
+		a = producir_dato() ; //creamos el dato
+		buffer[primera_celda_libre % tam_vec]= a; // lo escribimos en la primera posición libre del buffer (teniendo en cuenta que lo recorremos de manera circular)
+		primera_celda_libre++;
+		sem_signal(alguien_produciendo);
+
+		sem_signal(puede_consumir); //indicamos que ya se puede leer
+	}
+}
+
+
+//----------------------------------------------------------------------
+
+void funcion_hebra_consumidora_FIFO(){
+	int b;
+	while(consumidos < num_items){
+		mtx.lock();
+		consumidos++;
+		mtx.unlock();
+
+		sem_wait(alguien_consumiendo);
+
+		sem_wait(puede_consumir);
+		b= buffer[primera_celda_ocupada_FIFO % tam_vec];
+		primera_celda_ocupada_FIFO++;
+		consumir_dato(b) ;
+		sem_signal(puede_producir);
+
+		sem_signal(alguien_consumiendo);
+	}
+}
+//----------------------------------------------------------------------
+
+int main(){
+	int n, prods, cons;
+	cout << "--------------------------------------------------------" << endl
+			 << "Problema de n productores- n consumidores (solución FIFO)." << endl
+			 << "--------------------------------------------------------" << endl
+		   << flush ;
+	cout << "\nIntroduzca número deseado de productores y consumidores: ";
+	cin >> prods >> cons;
+
+	thread productores[prods], consumidores[cons];
+
+	for(unsigned i= 0; i< prods; i++)
+		productores[i]= thread(funcion_hebra_productora_FIFO);
+
+	for(unsigned i= 0; i< cons; i++)
+		consumidores[i]= thread(funcion_hebra_consumidora_FIFO);
+
+	for(unsigned i= 0; i< prods; i++)
+		productores[i].join();
+
+	for(unsigned i= 0; i< cons; i++)
+		consumidores[i].join();
+
+
+	test_contadores();
+}
